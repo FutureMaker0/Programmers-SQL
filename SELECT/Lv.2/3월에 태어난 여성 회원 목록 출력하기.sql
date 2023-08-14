e85c7a41619427681eed11a14412f67b40e50654
FutureMaker0 <120623320+FutureMaker0@users.noreply.github.com>
2023-08-14 17:06:12 +0900

    Create 3월에 태어난 여성 회원 목록 출력하기.sql

---
+++ SELECT/Lv.2/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -0,0 +1,9 @@
+SELECT
+    member_id,
+    member_name,
+    gender,
+    date_format(date_of_birth, '%Y-%m-%d')
+from member_profile
+where extract(month from date_of_birth) = 3
+and gender = 'W' and !isnull(tlno)
+order by member_id
